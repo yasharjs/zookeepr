db1eb5e4a924677eccf8619d05fcdd772c48005e
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2022-02-20 17:59:21 -0500

    fully functioning apo that can handle GET and POST requests

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const {animals} = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array
+app.use(express.urlencoded({extended: true}));
+// parse incoming json data
+app.use(express.json());
 
 
 
@@ -49,6 +56,36 @@ function findById(id,animalsArray){
     return result;
 }
 
+function createNewAnimal (body, animalsArray){
+    // console.log(body);
+    //our function's main code will go here
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !== 'string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+
+    return true;
+}
+
 app.get('/api/animals' , (req,res) => {
     let results = animals;
     if(req.query){
@@ -64,6 +101,29 @@ app.get('/api/animals/:id',(req,res) => {
         res.send(404);
     }
 })
+
+app.post('/api/animals',(req,res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.');
+    } else{
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body,animals);
+        res.json(animal);
+    }
+
+    
+})
+
+
+
+
 app.listen(PORT, ()=>{
     console.log(`API server now on port ${PORT}`);
 })
